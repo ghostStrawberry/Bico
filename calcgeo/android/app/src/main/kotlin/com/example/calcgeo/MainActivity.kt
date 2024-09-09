07bfee74a8f506d049f8ae41a4015158981dde16
ghostStrawberry <ari.carvalho1b.infi@gmail.com>
2024-09-09 19:42:01 -0300

    Esqueleto da Calculadora

---
+++ calcgeo/android/app/src/main/kotlin/com/example/calcgeo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.calcgeo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ rotas/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:rotas/telas/telaprincipal.dart';
+
+void main() {
+  runApp(const Rotas());
+}
